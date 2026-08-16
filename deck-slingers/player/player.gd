51fd39fd1e2b6838f56ffebca52bf80b541eacf8
web <146587835+ar-bh@users.noreply.github.com>
2026-08-16 18:03:43 -0400

    redliner basic movement mimicked

--- deck-slingers/player/player.gd
+++ deck-slingers/player/player.gd
@@ -3,82 +3,103 @@ class_name Player extends CharacterBody3D
 @onready var _head: Node3D = $Head
 @onready var _camera: Camera3D = %Camera3D
 
-@export var vertical_look_sensitivity: float = 0.006
-@export var horizontal_look_sensitivity: float = 0.006
-@export var vertical_controller_look_sensitivity: float = 0.03
-@export var horizontal_controller_look_sensitivity: float = 0.03
-
-
-@export var jump_velocity := 6.0
-@export var auto_bhop := true
+#region movement and camera settings
+@export_group("Ground Movement")
 @export var walk_speed := 7.0
 @export var sprint_speed := 8.5
+@export var jump_velocity := 6.0
+@export var auto_bhop = true
 
-@export_group("Headbob")
-@export var headbob_enabled := false
-@export var headbob_move_amount: = 0.06
-@export var headbob_frequency := 2.4
-@export var headbob_time := 0.0
+@export_group("Air Physics")
+@export var air_cap := 0.5 # the speed limit added per frame in the air
+@export var air_accel := 800.0 # acceleration multipler in the air
+@export var air_move_speed := 500.0
 
-var wish_dir := Vector3.ZERO
+@export_group("Mouse Look")
+@export var vertical_mouse_sensitivity := 0.003
+@export var horizontal_mouse_sensitivity := 0.003
 
-func get_move_speed() -> float:
-	return sprint_speed if Input.is_action_pressed("sprint") else walk_speed
+@export_group("Controller Look")
+@export var vertical_controller_sensitivity := 3.0
+@export var horizontal_controller_sensitivity := 3.0
+@export var controller_deadzone = 0.15
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+#endregion
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _unhandled_input(event) -> void:
+	
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			rotate_y(-event.relative.x * horizontal_look_sensitivity)
-			_camera.rotate_x(-event.relative.y * vertical_look_sensitivity)
-			_camera.rotation.x = clamp(_camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-
-func _headbob_effect(delta: float) -> void:
-	headbob_time += delta * velocity.length()
-	_camera.transform.origin = Vector3(
-		cos(headbob_time * headbob_frequency * 0.5) * headbob_move_amount,
-		sin(headbob_time * headbob_frequency) * headbob_move_amount,
-		0
-	)
-
-var _cur_controller_look := Vector2()
-func _handle_controller_look_input(delta: float) -> void:
-	var target_look := Input.get_vector("look_left", "look_right", "look_down", "look_up").normalized()
-	_cur_controller_look = target_look
-	
-	rotate_y(-_cur_controller_look.x * horizontal_controller_look_sensitivity)
-	_camera.rotate_x(_cur_controller_look.y * vertical_controller_look_sensitivity)
-	_camera.rotation.x = clamp(_camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
+		_handle_mouse_look(event.relative)
+		
 func _process(delta: float) -> void:
-	_handle_controller_look_input(delta)
-
-func _handle_air_physics(delta: float) -> void:
-	velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	_handle_controller_look(delta)
 	
-func _handle_ground_physics(delta: float) -> void:
-	velocity.x = wish_dir.x * get_move_speed()
-	velocity.z = wish_dir.z * get_move_speed()
-	
-	if headbob_enabled:
-		_headbob_effect(delta)
-
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
-	wish_dir = global_transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)
+	var wish_dir := _get_wish_dir()
 	
 	if is_on_floor():
-		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
-			velocity.y = jump_velocity
-		_handle_ground_physics(delta)
+		_handle_ground_physics(wish_dir)
 	else:
-		_handle_air_physics(delta)
+		_handle_air_physics(wish_dir, delta)
 		
 	move_and_slide()
+		
+#region camera control funcs
+func _handle_mouse_look(relative_motion: Vector2) -> void:
+	rotate_y(-relative_motion.x * horizontal_mouse_sensitivity)
+	_camera.rotate_x(-relative_motion.y * vertical_mouse_sensitivity)
+	_camera.rotation.x = clamp(_camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	
+func _handle_controller_look(delta: float) -> void:
+	var look_dir := Input.get_vector("look_left", "look_right", "look_up", "look_down").normalized()
+	
+	if look_dir.length() > controller_deadzone:
+		rotate_y(-look_dir.x * horizontal_controller_sensitivity * delta)
+		_camera.rotate_x(look_dir.y * vertical_controller_sensitivity * delta)
+		_camera.rotation.x = clamp(_camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+#endregion
+	
+#region movement funcs
+func _get_wish_dir() -> Vector3:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
+	if input_dir == Vector2.ZERO:
+		return Vector3.ZERO
+		
+	return (global_transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
+	
+func _handle_ground_physics(wish_dir: Vector3) -> void:
+	
+	var target_speed := sprint_speed if Input.is_action_pressed("sprint") else walk_speed	
+	
+	if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
+		velocity.y += jump_velocity
+		
+		var jump_dir := wish_dir if wish_dir != Vector3.ZERO else -global_transform.basis.z
+		velocity.x = jump_dir.x * target_speed
+		velocity.z = jump_dir.z * target_speed
+		
+	else:
+		velocity.x = wish_dir.x * target_speed
+		velocity.z = wish_dir.z * target_speed
+		
+func _handle_air_physics(wish_dir: Vector3, delta: float) -> void:
+	velocity.y -= gravity * delta
+	if wish_dir != Vector3.ZERO:
+		var cur_speed_in_wish_dir := velocity.dot(wish_dir)
+		var capped_speed = min((air_move_speed * wish_dir).length(), air_cap)
+		var add_speed_till_cap = capped_speed - cur_speed_in_wish_dir
+		
+		if add_speed_till_cap > 0.0:
+			var accel_speed := air_accel * air_move_speed * delta
+			accel_speed = min(accel_speed, add_speed_till_cap)
+			velocity += accel_speed * wish_dir
+
+#endregion
